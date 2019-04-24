bcf5b0f5903e5be31fd29430214383cf8394daa4
jmr-nist-gov <33733006+jmr-nist-gov@users.noreply.github.com>
2019-04-24 14:46:55 -0400

    Update server.R
    update package dependency handling

--- app/server.R
+++ app/server.R
@@ -1,6 +1,11 @@
 # Load dependencies
-x <- c("shiny", "tidyverse", "DT", "shinyjs", "openxlsx")
-lapply(x, require, character.only = TRUE)
+packs <- c("shiny", "tidyverse", "DT", "shinyjs", "openxlsx")
+packs_false <- packs[-which(packs %in% installed.packages())]
+if (length(packs_false) > 0) {
+  install.packages(pkgs = packs_false, dependencies = TRUE)
+}
+lapply(packs, library, character.only = TRUE)
+rm(packs, packs_false)
 
 # Version of Isotope Dilution Assistant to use
 source('IDA_functions.R')
