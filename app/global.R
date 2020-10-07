59a378d5a942c1607342274ffe54d05ac405cc98
Jared R <jared.ragland@nist.gov>
2020-10-07 16:00:41 -0400

    Directory clean up.

--- app/global.R
+++
@@ -1,5 +0,0 @@
-# Package compliance
-source("package_compliance.R")
-
-# Version of Isotope Dilution Assistant to use ----
-source('IDA_functions.R')
--- app/package_compliance.R
+++
@@ -1,9 +0,0 @@
-# These first two should really be moved to global.R
-# Load dependencies ----
-packs <- c("shiny", "tidyverse", "DT", "shinyjs", "openxlsx")
-packs_false <- packs[-which(packs %in% installed.packages())]
-if (length(packs_false) > 0) {
-  install.packages(pkgs = packs_false, dependencies = TRUE)
-}
-lapply(packs, library, character.only = TRUE)
-rm(packs, packs_false)
