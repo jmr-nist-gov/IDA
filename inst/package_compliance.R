c4e069f114972b4aed8d170080e8683e37226395
Jared R <jared.ragland@nist.gov>
2022-04-18 18:44:14 -0400

    Add ggrepel and scales 1.2 as dependencies.

--- inst/package_compliance.R
+++ inst/package_compliance.R
@@ -1,9 +1,12 @@
 # These first two should really be moved to global.R
 # Load dependencies ----
-packs <- c("shiny", "tidyverse", "DT", "shinyjs", "openxlsx")
+packs <- c("shiny", "tidyverse", "DT", "shinyjs", "openxlsx", "ggrepel", "scales")
 packs_false <- packs[-which(packs %in% installed.packages())]
 if (length(packs_false) > 0) {
   install.packages(pkgs = packs_false, dependencies = TRUE)
+}
+if (packageVersion("scales") < 1.2) {
+  install.packages("scales")
 }
 lapply(packs, library, character.only = TRUE)
 rm(packs, packs_false)
